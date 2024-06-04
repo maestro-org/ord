4b647aa999c48d6ef5f5c7eb9e2ebfb85060ed81
jmhrpr <25673452+jmhrpr@users.noreply.github.com>
2024-06-04 15:15:53 +0100

    pub types

--- src/inscriptions/envelope.rs
+++ src/inscriptions/envelope.rs
@@ -90,7 +90,7 @@ impl From<RawEnvelope> for ParsedEnvelope {
 }
 
 impl ParsedEnvelope {
-  pub(crate) fn from_transaction(transaction: &Transaction) -> Vec<Self> {
+  pub fn from_transaction(transaction: &Transaction) -> Vec<Self> {
     RawEnvelope::from_transaction(transaction)
       .into_iter()
       .map(|envelope| envelope.into())
